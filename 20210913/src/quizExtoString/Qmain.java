60330e8221299cbd70503bc3d8bead07f33cb0f2
kwonmin123123 <zzacjk@naver.com>
2021-09-13 10:26:03 +0900

    toStringQuiz

---
+++ 20210913/src/quizExtoString/Qmain.java
@@ -0,0 +1,40 @@
+package quizExtoString;
+
+public class Qmain {
+
+	public static void main(String[] args) {
+		// 아래의 Car 오브젝트에 쓰는 메쏘드 toString을 적절하게 오버라이딩해보자
+		// car1 = 테슬라 15000
+		// car2 =  기아 10000
+		//System.out.println(car1.toString());
+		//System.out.println(car2.toString());
+	
+		
+		
+		
+
+	}
+
+	class Car{
+		String name;
+		int price;
+	
+	
+	
+		Car(String name, int price) {
+			super();
+			this.name = name;
+			this.price = price;
+		}
+		public String toString() {
+			return "회사 : "+name+ "가격 : "+price+"달러";
+			
+		}
+	
+	
+		
+	}
+
+
+
+}
