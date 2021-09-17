4ee717c3daf13635017b8d0c6bd22388c6d620cd
kwonmin123123 <zzacjk@naver.com>
2021-09-17 11:42:29 +0900

    quiz05

---
+++ 20210917/src/setQuiz/quiz05.java
@@ -0,0 +1,27 @@
+package setQuiz;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class quiz05 {
+
+	
+	//중복된 글자가 몇종류인지 숫자를 반환하는 메쏘드
+	
+	
+	
+	public int dupLetter(String a) {
+		Set<String> set = new HashSet<String>();
+		Set<String> ret = new HashSet<String>();
+		String[] a1 = a.split("");
+		for (String t: a1) {
+			if (set.contains(t)) {
+				ret.add(t);
+			} else {
+				set.add(t);
+			}
+		}		
+		return ret.size();
+		
+	}
+}
