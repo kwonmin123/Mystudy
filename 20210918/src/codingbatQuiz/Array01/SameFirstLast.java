e311ce97cb024f8cfdd14d00751d859f6657600c
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 15:04:33 +0900

    samefirstLast sol

--- 20210918/src/codingbatQuiz/Array01/SameFirstLast.java
+++ 20210918/src/codingbatQuiz/Array01/SameFirstLast.java
@@ -2,4 +2,10 @@ package codingbatQuiz.Array01;
 
 public class SameFirstLast {
 
+	public boolean sameFirstLast(int[] nums) {
+	
+		if(nums.length==0) {return false;}
+		return (nums[0]==nums[nums.length-1]);
+	
+	}
 }
