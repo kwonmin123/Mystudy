8707f6f912168dc27425641bc8e5afe43d3be7e6
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 16:05:38 +0900

    MaxEnd3

---
+++ 20210918/src/codingbatQuiz/Array01/MaxEnd3.java
@@ -0,0 +1,14 @@
+package codingbatQuiz.Array01;
+
+public class MaxEnd3 {
+
+	public int[] maxEnd3(int[] nums) {
+		
+		int max =Math.max(nums[0], nums[2]);
+		int[] nums2 = {max,max,max};
+		
+		
+		return nums2;
+	}
+
+}
