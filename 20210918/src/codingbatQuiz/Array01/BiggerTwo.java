1887ff3d84fe7190587c9e10fa3356c099485d45
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 17:04:07 +0900

    Bigger Two

---
+++ 20210918/src/codingbatQuiz/Array01/BiggerTwo.java
@@ -0,0 +1,14 @@
+package codingbatQuiz.Array01;
+
+public class BiggerTwo {
+
+	public int[] biggerTwo(int[] a, int[] b) {
+		if(a[0]+a[1]>=b[0]+b[1]) {
+			return a;
+		}
+		return b;
+		
+		
+	}
+
+}
