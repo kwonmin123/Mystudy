d4b65fae4ddf4cc5bc5ccf8d467dd449414ac3ff
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 17:49:29 +0900

    Make 2

--- 20210918/src/codingbatQuiz/Array01/Make2.java
+++ 20210918/src/codingbatQuiz/Array01/Make2.java
@@ -1,5 +1,23 @@
 package codingbatQuiz.Array01;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 public class Make2 {
+
+	public int[] make2(int[] a, int[] b) {
+	ArrayList<Integer> list =new ArrayList<Integer>();
+	for(int i :a) {
+		list.add(i);
+	}
+	for(int i :b) {
+		list.add(i);
+	}
+	int[] ret = {list.get(0),list.get(1)};
+	return ret;
+	
+	
+	
+	}
 
 }
