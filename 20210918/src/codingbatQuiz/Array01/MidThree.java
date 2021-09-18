bc1f20a68bf3ade2d710ce30d8b53c55e1a31e62
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 17:20:07 +0900

    Mid Three

---
+++ 20210918/src/codingbatQuiz/Array01/MidThree.java
@@ -0,0 +1,9 @@
+package codingbatQuiz.Array01;
+
+public class MidThree {
+
+	public int[] midThree(int[] nums) {
+		  int[] ret= {nums[nums.length/2-1],nums[nums.length/2],nums[nums.length/2+1]};
+	return ret;
+	}
+}
