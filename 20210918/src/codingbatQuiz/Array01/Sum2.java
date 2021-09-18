00f67fe10e728f7121be6a76cf7b54ae8d4650b2
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 16:09:01 +0900

    Sum2

---
+++ 20210918/src/codingbatQuiz/Array01/Sum2.java
@@ -0,0 +1,15 @@
+package codingbatQuiz.Array01;
+
+public class Sum2 {
+
+	public int sum2(int[] nums) {
+	if(nums.length==0) {
+		return 0;
+		}
+	else if(nums.length==1){
+		return nums[0];
+	} else return nums[0]+nums[1];
+		
+		
+	}
+}
