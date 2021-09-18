31c88001c7077809b68280edff95e020e8a44d1e
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-09-18 16:52:40 +0900

    Fix23

---
+++ 20210918/src/codingbatQuiz/Array01/Fix23.java
@@ -0,0 +1,18 @@
+package codingbatQuiz.Array01;
+
+public class Fix23 {
+
+	public int[] fix23(int[] nums) {
+
+		if(nums[0]==2&&nums[1]==3) {
+			nums[1]=0;
+		}
+		if(nums[1]==2&&nums[2]==3) {
+			nums[2]=0;
+		}
+
+
+		return nums;
+	}
+
+}
