4bedce839635c70c18641c3f685d481d47d63a8c
kwonmin123123 <zzacjk@naver.com>
2021-09-19 22:13:26 +0900

    INTO 1 10

---
+++ 20210919/src/codingbatLogicQuiz/In1To10.java
@@ -0,0 +1,11 @@
+package codingbatLogicQuiz;
+
+public class In1To10 {
+
+	public boolean in1To10(int n, boolean outsideMode) {
+		  
+		  if(outsideMode){return n<=1||n>=10;}
+		  return (n>=1&&n<=10);
+		}
+
+}
