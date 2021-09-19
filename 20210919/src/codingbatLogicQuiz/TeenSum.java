ade3e03439bc103ad64fc82282c3531950f64bb6
kwonmin123123 <zzacjk@naver.com>
2021-09-19 22:32:58 +0900

    Teen Sum

---
+++ 20210919/src/codingbatLogicQuiz/TeenSum.java
@@ -0,0 +1,11 @@
+package codingbatLogicQuiz;
+
+public class TeenSum {
+
+	public int teenSum(int a, int b) {
+		  if(a>=13&&a<=19){return 19;}
+		  if(b>=13&&b<=19){return 19;}
+		  return a+b;
+		}
+
+}
