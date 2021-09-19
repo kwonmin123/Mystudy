062894c2ca7e17c0cc70c9b4cff3213558c9fe81
kwonmin123123 <zzacjk@naver.com>
2021-09-19 22:01:16 +0900

    Love  6

---
+++ 20210919/src/codingbatLogicQuiz/Love6.java
@@ -0,0 +1,12 @@
+package codingbatLogicQuiz;
+
+public class Love6 {
+
+	public boolean love6(int a, int b) {
+		  if(a==6||b==6){return true;}
+		  if(Math.abs(a-b)==6){return true;}
+		  if(a+b==6){return true;}
+		  else  return false;
+		}
+
+}
