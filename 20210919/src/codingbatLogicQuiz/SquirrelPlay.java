42bb0ef7b8e28c6429fcb11ca6d26ebe1608b9ce
kwonmin123123 <zzacjk@naver.com>
2021-09-19 21:42:28 +0900

    Squirrel Play

---
+++ 20210919/src/codingbatLogicQuiz/SquirrelPlay.java
@@ -0,0 +1,10 @@
+package codingbatLogicQuiz;
+
+public class SquirrelPlay {
+
+	public boolean squirrelPlay(int temp, boolean isSummer) {
+		  if(isSummer){return (temp>=60&&temp<=100);}
+		  else return(temp>=60&&temp<=90);
+		}
+
+}
