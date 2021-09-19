a50eda3dc7cdafa436746837c0ea130f760553b3
kwonmin123123 <zzacjk@naver.com>
2021-09-19 21:40:51 +0900

    DateFashion

---
+++ 20210919/src/codingbatLogicQuiz/DateFashion.java
@@ -0,0 +1,11 @@
+package codingbatLogicQuiz;
+
+public class DateFashion {
+
+	public int dateFashion(int you, int date) {
+		  if(you<=2||date<=2){return 0;}
+		  else if (you>=8||date>=8){return 2;}
+		  return 1;
+		}
+
+}
