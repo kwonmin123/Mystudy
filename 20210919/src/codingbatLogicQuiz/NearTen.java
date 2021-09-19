5662477a2bf23560f19b7f6ddb997f3e53077665
kwonmin123123 <zzacjk@naver.com>
2021-09-19 22:30:01 +0900

    Near Ten

---
+++ 20210919/src/codingbatLogicQuiz/NearTen.java
@@ -0,0 +1,10 @@
+package codingbatLogicQuiz;
+
+public class NearTen {
+
+	public boolean nearTen(int n) {
+		  if(n%10==0||n%10==1||n%10==2||n%10==9||n%10==8){return true;}
+		  return false;
+		}
+
+}
