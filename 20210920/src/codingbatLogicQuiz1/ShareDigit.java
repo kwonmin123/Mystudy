dc281ed6330aa34a4059f82be3444746a306c3e2
kwonmin123123 <zzacjk@naver.com>
2021-09-20 18:18:55 +0900

    ShareDigit sol2

--- 20210920/src/codingbatLogicQuiz1/ShareDigit.java
+++ 20210920/src/codingbatLogicQuiz1/ShareDigit.java
@@ -2,14 +2,15 @@ package codingbatLogicQuiz1;
 
 public class ShareDigit {
 	public boolean shareDigit(int a, int b) {
-		 String a1= a+""; 
-		 String b1= b+"";
-		 
-		 if(a1.charAt(0)==b1.charAt(0)||a1.charAt(0)==b1.charAt(1)) {return true;}
-		 if(a1.charAt(1)==b1.charAt(0)||a1.charAt(1)==b1.charAt(1)) {return true;}
-		 return false;
-	
-	
+//		 String a1= a+""; 
+//		 String b1= b+"";
+//		 
+//		 if(a1.charAt(0)==b1.charAt(0)||a1.charAt(0)==b1.charAt(1)) {return true;}
+//		 if(a1.charAt(1)==b1.charAt(0)||a1.charAt(1)==b1.charAt(1)) {return true;}
+//		 return false;
+if(a%10==b%10||a%10==b/10) {return true;}	
+if(a/10==b%10||a/10==b/10) {return true;}	
+	return false;
 	
 	}
 
