e25375377924b4788a49a27e66fa7e41ea4c47ef
kwonmin123123 <zzacjk@naver.com>
2021-09-20 18:28:17 +0900

    SumLimit

---
+++ 20210920/src/codingbatLogicQuiz1/SumLimit.java
@@ -0,0 +1,12 @@
+package codingbatLogicQuiz1;
+
+public class SumLimit {
+	public int sumLimit(int a, int b) {
+		  String a1= a+"";
+		  String ab= (a+b)+"";
+		  if(ab.length()!=a1.length()){return a;}
+		  
+		  return a+b;
+		}
+
+}
