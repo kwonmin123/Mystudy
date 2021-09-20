2adec95264a9b1e9bc199ef0b23fe4cfa9d62299
kwonmin123123 <zzacjk@naver.com>
2021-09-20 18:16:36 +0900

    ShareDigit

---
+++ 20210920/src/codingbatLogicQuiz1/ShareDigit.java
@@ -0,0 +1,16 @@
+package codingbatLogicQuiz1;
+
+public class ShareDigit {
+	public boolean shareDigit(int a, int b) {
+		 String a1= a+""; 
+		 String b1= b+"";
+		 
+		 if(a1.charAt(0)==b1.charAt(0)||a1.charAt(0)==b1.charAt(1)) {return true;}
+		 if(a1.charAt(1)==b1.charAt(0)||a1.charAt(1)==b1.charAt(1)) {return true;}
+		 return false;
+	
+	
+	
+	}
+
+}
