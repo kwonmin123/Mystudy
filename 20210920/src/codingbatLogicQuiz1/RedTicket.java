1bbe2d272d5f26a3d62cab42bd69c49a0f7eacd2
kwonmin123123 <zzacjk@naver.com>
2021-09-20 17:57:52 +0900

    Red Ticket

---
+++ 20210920/src/codingbatLogicQuiz1/RedTicket.java
@@ -0,0 +1,12 @@
+package codingbatLogicQuiz1;
+
+public class RedTicket {
+
+	public int redTicket(int a, int b, int c) {
+		  if(a==2&&b==2&&c==2){return 10;}
+		  if(a==b&&b==c){return 5;}
+		  if(a!=b&&a!=c){return 1;}
+		  return 0;
+		  
+		}
+}
