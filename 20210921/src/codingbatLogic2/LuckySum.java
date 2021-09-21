6c66a4f216a7c4b640f2f320ddc4e7c3d78ae50b
kwonmin123123 <zzacjk@naver.com>
2021-09-21 20:59:13 +0900

    LuckySum

---
+++ 20210921/src/codingbatLogic2/LuckySum.java
@@ -0,0 +1,11 @@
+package codingbatLogic2;
+
+public class LuckySum {
+	public int luckySum(int a, int b, int c) {
+		  if(a==13){return 0;}
+		  if(b==13){return a;}
+		  if(c==13){return a+b;}
+		  return a+b+c;
+		}
+
+}
