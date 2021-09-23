a239d23d0cacf899779630038be5efccc4da1d2f
kwonmin123123 <zzacjk@naver.com>
2021-09-23 10:21:07 +0900

    MapQuiz01 com

--- 20210923/src/mapQuiz/MapQuiz01.java
+++ 20210923/src/mapQuiz/MapQuiz01.java
@@ -1,5 +1,13 @@
 package mapQuiz;
 
+import java.util.Map;
+
 public class MapQuiz01 {
+
+	public void put(Map<Integer, Integer> map) {
+		map.put(1, 2);
+		map.put(2, 4);
+		
+	}
 
 }
--- 20210923/src/mapQuiz/MapQuiz01Test.java
+++ 20210923/src/mapQuiz/MapQuiz01Test.java
@@ -2,13 +2,24 @@ package mapQuiz;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 
 class MapQuiz01Test {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+	
+		MapQuiz01 e =new MapQuiz01();
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		e.put(map);
+		assertEquals(2, map.get(1));
+		assertEquals(4, map.get(2));
+		assertEquals(2, map.size());
+		assertFalse(map.containsKey(3));
 	}
 
 }
