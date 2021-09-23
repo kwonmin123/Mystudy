fa230e910315369aeb2199f11989ae28d6fc2230
kwonmin123123 <zzacjk@naver.com>
2021-09-23 10:40:02 +0900

    codingBatMapBully

---
+++ 20210923/src/codignBatMapQiuz/MapBully.java
@@ -0,0 +1,15 @@
+package codignBatMapQiuz;
+
+import java.util.Map;
+
+public class MapBully {
+
+	public Map<String, String> mapBully(Map<String, String> map) {
+		   if (map.containsKey("a")) {
+		    map.put("b", map.get("a"));
+		    map.put("a", "");
+		  }
+		  return map;
+		}
+
+}
