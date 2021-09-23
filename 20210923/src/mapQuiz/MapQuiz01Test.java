a2c4e99bf8985ccdf8ac079abc1fd2d289199ef3
kwonmin123123 <zzacjk@naver.com>
2021-09-23 10:10:09 +0900

    MapQuiz +Juint

---
+++ 20210923/src/mapQuiz/MapQuiz01Test.java
@@ -0,0 +1,14 @@
+package mapQuiz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MapQuiz01Test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
