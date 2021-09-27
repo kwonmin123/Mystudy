e016299e9961648822686e032bc7266923646f0d
kwonmin123123 <zzacjk@naver.com>
2021-09-27 22:28:43 +0900

    lamdaQuiz02

--- 20210927/src/lamdaQuiz/Ex2.java
+++ 20210927/src/lamdaQuiz/Ex2.java
@@ -4,10 +4,14 @@ public class Ex2 {
 	public static void main(String[] args) {
 		MyInterface2 o1;   // 적절한 코드 작성
 		MyInterface2 o2;   // 적절한 코드 작성
+		MyInterface2 o3;   // 적절한 코드 작성
 		o1= () -> System.out.println("hello");
 		o2= () -> System.out.println("java");
+		o3= () -> System.out.println("worlds");		
+		
 		o1.method(); // hello
 		o2.method(); // java
+		o3.method(); // java
 		
 		// 출력
 		// hello
