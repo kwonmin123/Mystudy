7457ae44cad3ef07d5f9f4b4ac2dc6b06a0e81a6
kwonmin123123 <zzacjk@naver.com>
2021-09-28 10:10:35 +0900

    foreachLamda

--- 20210928/src/lamdaQuiz/Ex01.java
+++ 20210928/src/lamdaQuiz/Ex01.java
@@ -6,10 +6,11 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		List<Integer> list = Arrays.asList(3, 4, 5, 6);
-	
-		method(list,num->System.out.println(num) ); // 코드 완성
-		method(list,num->System.out.println("item : "+num) ); // 코드 완성
-		method(list,num->System.out.println("item : "+num*2) ); // 코드 완성
+		list.forEach(a->System.out.print(a*2+" "));//
+		System.out.println();
+		method(list,num->System.out.println(num) ); // 코드 완성 3,4,5,6,출력코드
+		method(list,num->System.out.println("item : "+num) ); // 코드 완성 item : 3,4,5,6출력
+		method(list,num->System.out.println("item : "+num*2) ); // 코드 완성3456두배 출력
 	}
 	public static void method(List<Integer> list, MyInterface4 c) {
 		for (int item : list) {
