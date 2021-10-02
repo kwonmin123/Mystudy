136215b0650fbe5317698e00dd59dfde3cc452ad
kwonmin123 <89769630+kwonmin123@users.noreply.github.com>
2021-10-02 15:58:35 +0900

    Update Ex10.java

--- 20211002/src/codingBatString3/Ex10.java
+++ 20211002/src/codingBatString3/Ex10.java
@@ -1,5 +1,19 @@
 package codingBatString3;
 
+
+
 public class Ex10 {
+	public  int sumNumbers(String str) {
+		str =str.replaceAll("\\D", " ");
+		int sum = 0; 
+		String []a= str.split("[ ]+");
+		for(String s: a) {
+			if(s.length()!=0) {
+				sum+=Integer.parseInt(s);
+			}
+		}
+		return sum;
+	}
+
 
 }
