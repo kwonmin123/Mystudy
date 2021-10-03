04a9063d7982d286f18a9f051916e3ec480a1ca6
kwonmin123123 <zzacjk@naver.com>
2021-10-03 23:11:37 +0900

    Ex09

---
+++ 20211003/src/codingbatArray2/Ex09.java
@@ -0,0 +1,13 @@
+package codingbatArray2;
+
+public class Ex09 {
+	public int[] fizzArray(int n) {
+		  int[] ret =new int[n];
+		  
+		  for(int i=0;i<n;i++){
+		    ret[i]=i;
+		  }
+		  return ret;
+		}
+
+}
