df59d639ba6b457d063a90b03318e6e0a0b72b74
kwonmin123123 <zzacjk@naver.com>
2021-10-04 22:59:11 +0900

    Ex12

---
+++ 20211004/src/codingbatArray2/Ex12.java
@@ -0,0 +1,13 @@
+package codingbatArray2;
+
+public class Ex12 {
+	public String[] fizzArray2(int n) {
+		  String[] ret = new String[n];
+		  for(int i =0;i<n;i++){
+		    
+		    ret[i]=""+i;
+		  }
+		  return ret;
+		}
+
+}
